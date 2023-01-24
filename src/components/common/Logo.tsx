2a6e4760157356074e116247066d2ec07cd4dad7
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-01-24 23:44:05 +0600

    feat: change logo

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 
 export const Logo = () => (
   <Link href="/">
-    <img alt="NIS PM Almaty Logo" className="h-[70px] my-2" src="/logo.png" />
+    <img alt="NIS PM Almaty Logo" className="h-[48px] my-2" src="/logo.png" />
   </Link>
 );
