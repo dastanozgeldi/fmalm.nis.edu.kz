4faf9f33e604b218446320ce82eb38dec8991b2e
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-01-25 00:02:03 +0600

    feat: display topic && handle nullables

--- src/components/home/ArticleItem.tsx
+++ src/components/home/ArticleItem.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
-import { FaClock, FaUser } from "react-icons/fa";
+import { FaCalendar, FaUser } from "react-icons/fa";
 import Moment from "react-moment";
 import { getStrapiMedia } from "@/lib/media";
 import Image from "next/image";
 
 export const ArticleItem = ({ article }: any) => {
-  const { slug, image, title, description, published_at, author } =
+  const { slug, image, title, description, published_at, author, topic } =
     article.attributes;
 
   return (
     <Link href={`/article/${slug}`} className="m-4">
-      <div className="border-2 rounded-lg max-w-[48ch]">
+      <div className="relative border-2 rounded-lg w-[360px]">
+        {topic.data && (
+          <span className="text-sm absolute right-0 p-2 m-2 text-white bg-secondary rounded-lg">
+            {topic.data.attributes.name}
+          </span>
+        )}
         {image.data && (
           <Image
-            className="w-[48ch] h-[24ch] object-cover"
-            width={480}
+            className="w-[360px] h-[240px] object-cover rounded-t-lg"
+            width={400}
             height={240}
             src={getStrapiMedia(image)}
             alt="Banner Image"
@@ -22,17 +27,19 @@ export const ArticleItem = ({ article }: any) => {
         )}
         <div className="p-2">
           <h2 className="text-xl font-semibold">{title}</h2>
-          <p className="text-gray-500">{description}</p>
+          <p className="text-gray-500">{description.slice(0, 50)} ...</p>
         </div>
-        <div className="px-2 flex space-x-4 my-2">
+        <div className="px-2 flex items-center justify-between space-x-4 my-2">
           <p className="text-gray-500 flex items-center gap-2">
-            <FaClock />
+            <FaCalendar />
             <Moment format="MMM Do YYYY">{published_at}</Moment>
           </p>
-          <p className="text-gray-500 flex items-center gap-2">
-            <FaUser />
-            {author.attributes && author.attributes.name}
-          </p>
+          {author.data && (
+            <p className="text-gray-500 flex items-center gap-2">
+              <FaUser />
+              {author.data?.attributes.name}
+            </p>
+          )}
         </div>
       </div>
     </Link>
