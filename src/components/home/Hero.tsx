2d97eda0227af595368d7d82b66db6db58f8383b
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-02-01 20:42:22 +0600

    fix: content in container

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,14 +1,12 @@
-export const Hero = () => {
-  return (
-    <div className="relative flex items-center justify-center h-screen overflow-hidden">
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
-      >
-        <source src="nis.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
-};
+export const Hero = () => (
+  <div className="relative flex items-center justify-center h-screen overflow-hidden">
+    <video
+      autoPlay
+      loop
+      muted
+      className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+    >
+      <source src="nis.mp4" type="video/mp4" />
+    </video>
+  </div>
+);
