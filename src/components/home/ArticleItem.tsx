b23059df86f084495f2ede63e6eb5d85e4c7e157
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-02-02 07:38:50 +0600

    feat: redesign articles

--- src/components/home/ArticleItem.tsx
+++ src/components/home/ArticleItem.tsx
@@ -3,45 +3,52 @@ import { FaCalendar, FaUser } from "react-icons/fa";
 import Moment from "react-moment";
 import { getStrapiMedia } from "@/lib/media";
 import Image from "next/image";
+import { IoArrowForward } from "react-icons/io5";
 
-export const ArticleItem = ({ article }: any) => {
+export const ArticleItem = ({ article, read_more }: any) => {
   const { slug, image, title, description, published_at, author, topic } =
     article.attributes;
 
   return (
-    <Link href={`/article/${slug}`} className="m-4">
-      <div className="relative border-2 rounded-lg w-[360px]">
-        {topic?.data && (
-          <span className="text-sm absolute right-0 p-2 m-2 text-white bg-secondary rounded-lg">
-            {topic.data.attributes.name}
-          </span>
-        )}
-        {image?.data && (
-          <Image
-            className="w-[360px] h-[360px] object-cover rounded-t-lg"
-            width={360}
-            height={360}
-            src={getStrapiMedia(image)}
-            alt="Banner Image"
-          />
-        )}
-        <div className="p-2">
-          <h2 className="text-xl font-semibold">{title}</h2>
-          <p className="text-gray-500">{description.slice(0, 50)} ...</p>
-        </div>
-        <div className="px-2 flex items-center justify-between space-x-4 my-2">
-          <p className="text-gray-500 flex items-center gap-2">
-            <FaCalendar />
-            <Moment format="MMM Do YYYY">{published_at}</Moment>
+    <div className="relative shadow rounded-lg w-[320px]">
+      {topic?.data && (
+        <span className="text-sm absolute right-0 p-2 m-2 text-white bg-secondary rounded-lg">
+          {topic.data.attributes.name}
+        </span>
+      )}
+      {image?.data && (
+        <Image
+          className="w-[320px] h-[240px] object-cover rounded-t-lg"
+          width={320}
+          height={240}
+          src={getStrapiMedia(image)}
+          alt="Banner Image"
+        />
+      )}
+      <div className="pt-2 px-4 flex items-center justify-between space-x-4 my-2">
+        <p className="text-sm text-gray-500 flex items-center gap-2">
+          <FaCalendar />
+          <Moment format="MMM Do YYYY">{published_at}</Moment>
+        </p>
+        {author?.data && (
+          <p className="text-sm text-gray-500 flex items-center gap-2">
+            <FaUser />
+            {author.data.attributes.name}
           </p>
-          {author?.data && (
-            <p className="text-gray-500 flex items-center gap-2">
-              <FaUser />
-              {author.data.attributes.name}
-            </p>
-          )}
-        </div>
+        )}
       </div>
-    </Link>
+      <div className="px-4">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <p className="text-md text-gray-500">{description.slice(0, 50)} ...</p>
+      </div>
+      <div className="py-3 px-4">
+        <Link
+          href={`/article/${slug}`}
+          className="text-sm flex items-center w-max gap-2 button-secondary"
+        >
+          {read_more} <IoArrowForward />
+        </Link>
+      </div>
+    </div>
   );
 };
--- src/components/home/Articles.tsx
+++ src/components/home/Articles.tsx
@@ -16,17 +16,21 @@ export const Articles = ({
   const t = useTranslations("Articles");
 
   return (
-    <section className="p-8">
-      <h1 className="text-center text-4xl heading">{children}</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 items-center justify-items-center">
+    <section className="p-8 space-y-8">
+      <h1 className="text-center text-3xl heading">{children}</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-center justify-items-center">
         {articles.map((article: any) => (
-          <ArticleItem key={article.id} article={article} />
+          <ArticleItem
+            key={article.id}
+            article={article}
+            read_more={t("read_more")}
+          />
         ))}
       </div>
       <div className="flex items-center justify-center">
         {/* if there are less than 3 articles, there is no point in navigating to "all articles" */}
         {articles.length >= 3 && showMore && (
-          <Link href="/articles" className="button-primary">
+          <Link href="/articles" className="button-secondary">
             {t("show_more")}
           </Link>
         )}
