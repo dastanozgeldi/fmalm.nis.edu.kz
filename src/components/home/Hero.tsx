e6857e4322f116285e60d2c917ca0eca8814aa63
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-02-27 16:12:55 +0600

    feat: custom image carousel

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import { FaChevronRight } from "react-icons/fa";
+import { ImageCarousel } from "./ImageCarousel";
 
 export const Hero = () => {
   const t = useTranslations("Hero");
@@ -7,27 +8,16 @@ export const Hero = () => {
   return (
     <>
       <div className="relative">
-        <video
-          loop
-          autoPlay
-          muted
-          className="absolute brightness-50 w-full h-screen object-cover"
-        >
-          <source
-            className="w-full h-screen rounded"
-            src="what-is-nis.mp4"
-            type="video/mp4"
-          />
-        </video>
+        <ImageCarousel />
         <div className="h-screen relative lg:left-20 flex items-center px-4 lg:w-1/2">
-          <div className="space-y-3">
+          <div className="space-y-3 p-6 lg:p-0">
             <h1 className="text-white text-3xl lg:text-5xl font-extrabold">
               {t("title")}
             </h1>
             <p className="text-white text-lg lg:text-xl">{t("description")}</p>
           </div>
         </div>
-        <div className="absolute right-0 bottom-0 flex pt-6">
+        <div className="absolute right-4 bottom-0 flex">
           <a
             href="https://instagram.com/nis_pm_almaty"
             className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
---
+++ src/components/home/ImageCarousel.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+export const ImageCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slides = [
+    {
+      url: "https://images.unsplash.com/photo-1677350787420-d3309b8a1eb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+    {
+      url: "https://plus.unsplash.com/premium_photo-1669825050519-0f8c3f192285?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1677350840522-ad55f426d048?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1677449561844-37c17761c11e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+  ];
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  return (
+    <div className="brightness-50 max-w-full h-[780px] w-full m-auto px-4 absolute group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      >
+        {/* Left Arrow */}
+        <div className="absolute top-10 lg:top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* Right Arrow */}
+        <div className="absolute top-10 lg:top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+        <div className="flex bottom-4 justify-center py-2">
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="text-white text-2xl cursor-pointer"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
