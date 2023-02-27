11bbaf8b908a82b106531059f682e814a4a0fe7e
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-02-27 17:16:40 +0600

    fix: carousel improvements

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import { FaChevronRight } from "react-icons/fa";
 import { ImageCarousel } from "./ImageCarousel";
 
 export const Hero = () => {
@@ -16,29 +15,6 @@ export const Hero = () => {
             </h1>
             <p className="text-white text-lg lg:text-xl">{t("description")}</p>
           </div>
-        </div>
-        <div className="absolute right-4 bottom-0 flex">
-          <a
-            href="https://instagram.com/nis_pm_almaty"
-            className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
-          >
-            Instagram
-            <FaChevronRight className="w-3 h-3" />
-          </a>
-          <a
-            href="https://www.facebook.com/fmalm.nis.edu.kz/"
-            className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
-          >
-            Facebook
-            <FaChevronRight className="w-3 h-3" />
-          </a>
-          <a
-            href="https://www.youtube.com/@nis_pm_almaty"
-            className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
-          >
-            Youtube
-            <FaChevronRight className="w-3 h-3" />
-          </a>
         </div>
       </div>
     </>
--- src/components/home/ImageCarousel.tsx
+++ src/components/home/ImageCarousel.tsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { FaChevronRight } from "react-icons/fa";
 import { RxDotFilled } from "react-icons/rx";
 
 export const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const slides = [
     {
-      url: "https://images.unsplash.com/photo-1677350787420-d3309b8a1eb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+      url: "https://i.imgur.com/DcWxz66.jpg",
     },
     {
-      url: "https://plus.unsplash.com/premium_photo-1669825050519-0f8c3f192285?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60",
+      url: "https://i.imgur.com/duQOR2O.jpg",
     },
     {
-      url: "https://images.unsplash.com/photo-1677350840522-ad55f426d048?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      url: "https://i.imgur.com/noDMWxs.jpg",
     },
     {
-      url: "https://images.unsplash.com/photo-1677449561844-37c17761c11e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+      url: "https://i.imgur.com/ZkzYKNU.jpg",
+    },
+    {
+      url: "https://i.imgur.com/5Qz6snh.jpg",
+    },
+    {
+      url: "https://i.imgur.com/0TudJAN.jpg",
+    },
+    {
+      url: "https://i.imgur.com/77XniZ3.jpg",
+    },
+    {
+      url: "https://i.imgur.com/GzE7l2A.jpg",
+    },
+    {
+      url: "https://i.imgur.com/uKOXqu6.jpg",
+    },
+    {
+      url: "https://i.imgur.com/XOFJa5P.jpg",
     },
   ];
 
@@ -44,29 +63,54 @@ export const ImageCarousel = () => {
   }, [currentIndex]);
 
   return (
-    <div className="brightness-50 max-w-full h-[780px] w-full m-auto px-4 absolute group">
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-      >
-        {/* Left Arrow */}
-        <div className="absolute top-10 lg:top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+    <div className="relative">
+      <div className="max-w-full h-[780px] w-full m-auto px-4 absolute group">
+        <div
+          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          className="brightness-50 w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        >
+          {/* Left Arrow */}
+          <div className="absolute top-10 lg:top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            <BsChevronCompactLeft onClick={prevSlide} size={30} />
+          </div>
+          {/* Right Arrow */}
+          <div className="absolute top-10 lg:top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            <BsChevronCompactRight onClick={nextSlide} size={30} />
+          </div>
+          <div className="flex bottom-4 justify-center py-2">
+            {slides.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+                className="text-white text-2xl cursor-pointer"
+              >
+                <RxDotFilled />
+              </div>
+            ))}
+          </div>
         </div>
-        {/* Right Arrow */}
-        <div className="absolute top-10 lg:top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        <div className="flex bottom-4 justify-center py-2">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className="text-white text-2xl cursor-pointer"
-            >
-              <RxDotFilled />
-            </div>
-          ))}
+        <div className="absolute bottom-0 right-4 flex">
+          <a
+            href="https://instagram.com/nis_pm_almaty"
+            className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
+          >
+            Instagram
+            <FaChevronRight className="w-3 h-3" />
+          </a>
+          <a
+            href="https://www.facebook.com/fmalm.nis.edu.kz/"
+            className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
+          >
+            Facebook
+            <FaChevronRight className="w-3 h-3" />
+          </a>
+          <a
+            href="https://www.youtube.com/@nis_pm_almaty"
+            className="hover:bg-opacity-90 hover:duration-300 flex items-center gap-2 p-3 bg-primary text-white"
+          >
+            Youtube
+            <FaChevronRight className="w-3 h-3" />
+          </a>
         </div>
       </div>
     </div>
