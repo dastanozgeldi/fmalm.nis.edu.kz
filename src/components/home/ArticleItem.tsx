3c824c8082e27d248f93804227c3009d8fef1a83
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-03-02 20:11:25 +0600

    fix: article item size when no caption

--- src/components/home/ArticleItem.tsx
+++ src/components/home/ArticleItem.tsx
@@ -2,26 +2,36 @@ import Link from "next/link";
 import { FaCalendar, FaUser } from "react-icons/fa";
 import { getStrapiMedia } from "@/lib/media";
 import Image from "next/image";
+import clsx from "clsx";
 
 export const ArticleItem = ({ article }: any) => {
   const { slug, image, title, description, createdAt, author, topic } =
     article.attributes;
 
+  const captionExists = image.data ? true : false;
+
   return (
-    <div className="relative hover:duration-500 border border-gray-200 rounded-lg min-w-[300px] max-w-[300px] min-h-[400px]">
-      {topic?.data && (
-        <span className="absolute top-2 right-2 text-sm py-2 px-4 text-white bg-secondary rounded-full">
-          #{topic.data.attributes.name}
-        </span>
+    <div
+      className={clsx(
+        "relative hover:duration-500 border border-gray-200 rounded-lg min-w-[300px] max-w-[300px]",
+        captionExists ? "min-h-[400px]" : "min-h-[175px]"
       )}
-      {image?.data && (
-        <Image
-          className="w-[300px] h-[225px] object-cover rounded-t-lg"
-          width={300}
-          height={225}
-          src={getStrapiMedia(image)}
-          alt="Banner Image"
-        />
+    >
+      {captionExists && (
+        <>
+          <Image
+            className="w-[300px] h-[225px] object-cover rounded-t-lg"
+            width={300}
+            height={225}
+            src={getStrapiMedia(image)}
+            alt="Banner Image"
+          />
+          {topic?.data && (
+            <span className="absolute top-2 right-2 text-sm py-2 px-4 text-white bg-secondary rounded-full">
+              #{topic.data.attributes.name}
+            </span>
+          )}
+        </>
       )}
       <div className="pt-2 px-4 flex items-center justify-between space-x-4 my-2">
         <p className="text-sm text-gray-500 flex items-center gap-2">
