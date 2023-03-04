8767cf1db048083a1a07a0a2cbc6d1ebebdb398a
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-03-04 14:21:53 +0600

    fix: footer alignment on smaller screens

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -18,25 +18,27 @@ export const Footer = () => {
       {/* top */}
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center gap-4">
-          <div className="w-full space-y-2 text-center flex flex-col items-center">
-            <Logo />
-            <p>{t("address")}</p>
-            <p>8 (727) 331 0104</p>
-            <p>info@fmalm.nis.edu.kz</p>
-            {/* Social Media */}
-            <div className="flex gap-2 py-4">
-              <a href="https://www.facebook.com/fmalm.nis.edu.kz">
-                <FaFacebook className="w-7 h-7" />
-              </a>
-              <a href="https://www.instagram.com/nis_pm_almaty/">
-                <FaInstagram className="w-7 h-7" />
-              </a>
-              <a href="https://www.youtube.com/@nis_pm_almaty">
-                <FaYoutube className="w-7 h-7" />
-              </a>
+          <div className="w-[80%] lg:w-full flex items-center">
+            <div className="space-y-2">
+              <Logo />
+              <p>{t("address")}</p>
+              <p>8 (727) 331 0104</p>
+              <p>info@fmalm.nis.edu.kz</p>
+              {/* Social Media */}
+              <div className="flex gap-2 py-4">
+                <a href="https://www.facebook.com/fmalm.nis.edu.kz">
+                  <FaFacebook className="w-5 h-5" />
+                </a>
+                <a href="https://www.instagram.com/nis_pm_almaty/">
+                  <FaInstagram className="w-5 h-5" />
+                </a>
+                <a href="https://www.youtube.com/@nis_pm_almaty">
+                  <FaYoutube className="w-5 h-5" />
+                </a>
+              </div>
             </div>
           </div>
-          <div className="w-full space-y-4 flex justify-center">
+          <div className="w-[80%] lg:w-full space-y-4 flex lg:justify-center">
             <div>
               <h2 className="heading">{t("useful_links")}</h2>
               <div className="space-y-2">
@@ -67,7 +69,7 @@ export const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="w-full space-y-4 flex justify-center">
+          <div className="w-[80%] lg:w-full space-y-4 flex lg:justify-center">
             <div>
               <h2 className="heading">{t("ecosystem")}</h2>
               <div className="space-y-2 flex flex-col">
@@ -86,7 +88,7 @@ export const Footer = () => {
         </div>
       </div>
       {/* bottom */}
-      <div className="bg-[#111] p-4 mt-4 text-sm text-gray-300 flex flex-col items-center justify-center space-y-2">
+      <div className="mx-auto w-[80%] lg:w-full bg-[#111] p-4 mt-4 text-sm text-gray-300 flex flex-col items-center justify-center space-y-2">
         <p className="text-gray-300 text-center">{t("copyright")}</p>
       </div>
     </footer>
