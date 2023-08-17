0697cd71560c802bba818f9f5f31459ef66b2e50
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-08-17 14:15:50 +0600

    feat: no author email publicly

--- src/components/post-author.tsx
+++ src/components/post-author.tsx
@@ -1,19 +1,13 @@
 import { Icons } from "./icons";
 
 export const PostAuthor = ({ author }: any) => {
-  const { name, email } = author.data.attributes;
-
+  const { name } = author.data.attributes;
   return (
-    <a
-      href={`mailto:${email}`}
-      key={author.id}
-      className="flex items-center space-x-2 text-sm"
-    >
-      <Icons.user size={32} />
+    <div key={author.id} className="flex items-center space-x-2 text-sm">
+      <Icons.user size={24} />
       <div className="flex-1 text-left leading-tight">
         <p className="font-medium">{name}</p>
-        <p className="text-[12px] text-muted-foreground">{email}</p>
       </div>
-    </a>
+    </div>
   );
 };
