85aae2697ed684e9bc62142939908f3613421cf2
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-08-17 20:14:27 +0600

    feat: change logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,10 +5,10 @@ export const Logo = () => (
   <Link href="/">
     <Image
       alt="NIS PM Almaty Logo"
-      src="/logo.png"
-      width={98}
-      height={36}
-      className="w-[98px] h-[36px] sm:w-[134px] sm:h-[48px] lg:w-[195px] lg:h-[72px] my-2"
+      src="/logo.svg"
+      width={96}
+      height={48}
+      className="w-[128px] h-[64px] lg:w-[200px] lg:h-[100px] my-2"
     />
   </Link>
 );
