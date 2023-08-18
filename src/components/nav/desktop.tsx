45d7249c5813c03bf96ba303719c6de16e37dd55
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-08-18 13:33:29 +0600

    fix: no nested <a> tags

--- src/components/nav/desktop.tsx
+++ src/components/nav/desktop.tsx
@@ -4,24 +4,31 @@ import { MenuIcons } from "./menu-icons";
 import { Icons } from "../icons";
 import { ChangeLanguageButton } from "./change-language";
 
-const MenuItem = ({ title, pages, href }: any) => (
-  <Link href={href}>
-    <button className="uppercase w-full lg:text-lg text-dark lg:w-max flex items-center justify-between gap-2 peer p-2">
-      {title} {pages.length > 0 && <Icons.down className="w-4 h-4" />}
-    </button>
-    <div className="w-full text-dark lg:w-max absolute z-50 hidden peer-hover:flex hover:flex flex-col bg-white drop-shadow-lg">
-      {pages.map((page: any) => (
-        <Link
-          key={page.attributes.id}
-          className="px-5 py-3 bg-gray-50 hover:bg-gray-200"
-          href={`/${page.attributes.slug}`}
-        >
-          {page.attributes.title}
-        </Link>
-      ))}
+const MenuItem = ({ title, pages, href }: any) =>
+  pages.length > 0 ? (
+    <div>
+      <button className="uppercase w-full lg:text-lg text-dark lg:w-max flex items-center justify-between gap-2 peer p-2">
+        {title} {pages.length > 0 && <Icons.down className="w-4 h-4" />}
+      </button>
+      <div className="w-full text-dark lg:w-max absolute z-50 hidden peer-hover:flex hover:flex flex-col bg-white drop-shadow-lg">
+        {pages.map((page: any) => (
+          <Link
+            key={page.id}
+            className="px-5 py-3 bg-gray-50 hover:bg-gray-200"
+            href={`/${page.attributes.slug}`}
+          >
+            {page.attributes.title}
+          </Link>
+        ))}
+      </div>
     </div>
-  </Link>
-);
+  ) : (
+    <Link href={href}>
+      <button className="uppercase w-full lg:text-lg text-dark lg:w-max flex items-center justify-between gap-2 peer p-2">
+        {title} {pages.length > 0 && <Icons.down className="w-4 h-4" />}
+      </button>
+    </Link>
+  );
 
 const Menu = ({ categories }: { categories: any[] }) => {
   return (
