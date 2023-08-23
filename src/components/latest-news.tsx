ac627e747c6032ff7ee6ccabed7720ff6782f0f4
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-08-23 13:38:13 +0600

    feat: add hover darkness to latest news title

--- src/components/latest-news.tsx
+++ src/components/latest-news.tsx
@@ -1,9 +1,65 @@
-import { getStrapiMedia } from "@/lib/media";
-import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
 import { ArticleCore } from "@/types";
+import { getStrapiMedia } from "@/lib/media";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+
+const FirstNewsItem = ({
+  first,
+  more,
+}: {
+  first: ArticleCore;
+  more: string;
+}) => {
+  const { image, title, createdAt, description, slug } = first.attributes;
+  return (
+    <div className="flex min-w-[300px] h-full justify-between text-white">
+      {image.data && (
+        <img
+          className="object-cover brightness-75"
+          src={getStrapiMedia(image)}
+          alt={title}
+        />
+      )}
+      <time className="absolute text-sm text-white top-0 right-0 p-6">
+        {new Date(createdAt).toLocaleDateString()}
+      </time>
+      <div className="absolute bottom-0 p-6 space-y-3">
+        <h3 className="font-bold text-lg md:text-2xl">{title}</h3>
+        <p className="hidden md:block">{description}</p>
+        <Link className={cn(buttonVariants())} href={`/articles/${slug}`}>
+          {more}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const LatestNewsItem = ({ item }: { item: ArticleCore }) => {
+  const { image, title, createdAt, slug } = item.attributes;
+  return (
+    <div key={item.id} className="relative overflow-hidden p-2 bg-gray-100">
+      <div className="flex min-w-[300px] gap-3 p-3">
+        {image.data && (
+          <img
+            className="w-[80px] h-[60px] object-cover"
+            src={getStrapiMedia(image)}
+            alt={title}
+          />
+        )}
+        <div>
+          <h3 className="font-bold text-lg hover:text-foreground/70 hover:duration-300">
+            <Link href={`/articles/${slug}`}>{title}</Link>
+          </h3>
+          <time className="text-sm text-gray-600">
+            {new Date(createdAt).toLocaleDateString()}
+          </time>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const LatestNews = ({ news }: { news: ArticleCore[] }) => {
   const t = useTranslations("LatestNews");
@@ -23,60 +79,12 @@ export const LatestNews = ({ news }: { news: ArticleCore[] }) => {
         </div>
         <div className="mx-auto justify-center flex flex-col lg:flex-row">
           <div className="relative w-full flex-1 lg:w-2/3">
-            <div className="flex min-w-[300px] h-full justify-between text-white">
-              {first.attributes.image.data && (
-                <img
-                  className="object-cover brightness-75"
-                  src={getStrapiMedia(first.attributes.image)}
-                  alt={first.attributes.title}
-                />
-              )}
-              <time className="absolute text-sm text-white top-0 right-0 p-6">
-                {new Date(first.attributes.createdAt).toLocaleDateString()}
-              </time>
-              <div className="absolute bottom-0 p-6 space-y-3">
-                <h3 className="font-bold text-lg md:text-2xl">
-                  {first.attributes.title}
-                </h3>
-                <p className="hidden md:block">
-                  {first.attributes.description}
-                </p>
-                <Link
-                  className={cn(buttonVariants())}
-                  href={`/articles/${first.attributes.slug}`}
-                >
-                  {t("more")}
-                </Link>
-              </div>
-            </div>
+            <FirstNewsItem first={first} more={t("more")} />
           </div>
 
           <div className="divide-y flex-1 divide-gray-400">
             {remaining.map((item) => (
-              <div
-                key={item.id}
-                className="relative overflow-hidden p-2 bg-gray-100"
-              >
-                <div className="flex min-w-[300px] gap-3 p-3">
-                  {item.attributes.image.data && (
-                    <img
-                      className="w-[80px] h-[60px] object-cover"
-                      src={getStrapiMedia(item.attributes.image)}
-                      alt={item.attributes.title}
-                    />
-                  )}
-                  <div>
-                    <h3 className="font-bold text-lg">
-                      <Link href={`/articles/${item.attributes.slug}`}>
-                        {item.attributes.title}
-                      </Link>
-                    </h3>
-                    <time className="text-sm text-gray-600">
-                      {new Date(item.attributes.createdAt).toLocaleDateString()}
-                    </time>
-                  </div>
-                </div>
-              </div>
+              <LatestNewsItem key={item.id} item={item} />
             ))}
           </div>
         </div>
