ba1ab0842a6bb8803c16a4cbbd2b602ca1a1b2bd
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-09-01 15:38:53 +0300

    feat: add analytics to school website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import { NextIntlProvider } from "next-intl";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextIntlProvider timeZone="Asia/Almaty" messages={pageProps.messages}>
       <Component {...pageProps} />
+      <Analytics />
     </NextIntlProvider>
   );
 }
